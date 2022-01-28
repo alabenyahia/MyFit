02935a03380991bb3c60842a3946c657a6e27a4d
Ala Ben Yahia <alabny18@gmail.com>
2022-01-28 23:28:53 +0100

    Edit theme colors and font family

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,12 @@ export let theme = createTheme({
     palette: {
         primary: {
             main: "#0090DC"
+        },
+        secondary: {
+            main: "#FF4650"
         }
+    },
+    typography: {
+        fontFamily: "'Inter', 'Roboto', 'sans-serif'"
     }
 })
