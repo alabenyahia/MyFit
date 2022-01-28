ccfb0c80a7d8a3899767ade6a0234f2cfe316b12
Ala Ben Yahia <alabny18@gmail.com>
2022-01-29 00:30:38 +0100

    Finish Login page UI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import TopAppBar from "./components/TopAppBar";
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme"
+import Login from "./pages/Login";
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <div className="App">
                 <TopAppBar/>
+                <Login/>
             </div>
         </ThemeProvider>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./css/Login.css";
+import Typography from "@mui/material/Typography";
+import {Card, CardContent, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
 
 const Login = () => {
+    const [email, setEmail] = useState()
+    const [pw, setPw] = useState()
     return (
         <div className="Login">
+            <Card sx={{maxWidth: "500px", margin: "50px auto"}}>
+                <CardContent>
+                    <Typography variant="h2" sx={{fontSize: "36px", fontWeight: "500"}} my={3} align="center">
+                        Login
+                    </Typography>
+                    <form>
+                        <TextField id="email"
+                                   label="Email address" fullWidth
+                                   sx={{marginBottom: "20px"}} value={email}
+                        onChange={(e) => setEmail(e.target.value)}/>
+                        <TextField id="password"
+                                   label="Password" fullWidth
+                                   value={pw}
+                                   type="password"
+                                   onChange={(e) => setPw(e.target.value)}/>
 
+                        <Button variant="contained" type="submit" fullWidth
+                                sx={{marginTop: "24px", marginBottom: "8px"}}>Login</Button>
+                        <Typography variant="p" sx={{fontSize: "12px", fontWeight: "400"}}>
+                            Not a member?<span style={{color: "#0090DC"}}> Register now!</span>
+                        </Typography>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 };
